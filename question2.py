633c38140a5fbf1976847bfd446fad39127ead6a
Aman Kshetri <79894697+aman-chhetri@users.noreply.github.com>
2023-02-27 15:06:14 +0545

    Update question2.py

--- question2.py
+++ question2.py
@@ -9,13 +9,13 @@ def is_prime(n):
 
 # We are creating unction to calculate number of valid bracelets of length n
 def count_bracelets(n):
-    x = [[0, 0] for _ in range(n+1)]
-    x[1][0] = x[1][1] = 1
+    arr = [[0, 0] for _ in range(n+1)]
+    arr[1][0] = arr[1][1] = 1
     for i in range(2, n+1):
         if is_prime(i):
-            x[i][0] = x[i-1][0] + x[i-1][1]
-            x[i][1] = x[i-1][1]
-    return x[n][0] + x[n][1]
+            arr[i][0] = arr[i-1][0] + arr[i-1][1]
+            arr[i][1] = arr[i-1][1]
+    return arr[n][0] + arr[n][1]
 
 # main function
 if __name__ == '__main__':
@@ -23,3 +23,10 @@ if __name__ == '__main__':
     for i in range(t):
         n = int(input("\nEnter the number of beads in bracelet: "))
         print("Number of bracelets possible: ",count_bracelets(n))
+
+
+
+# Explanation:
+
+# We are creating a 2D array dp where arr[i][j] represents the number of valid bracelets of length i that end with j white beads. Here, j can be either 0 or 1, representing black and white beads respectively.
+
